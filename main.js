f0ee4621cc565b6f84d56532618749e949f32cff
lucaasfacanha <lucaspereirafacanha@gmail.com>
2023-11-17 10:31:57 -0300

    novo projeto

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+function nomeIdade(nome, idade){
+    alert(`Nome: ${n}, Idade: ${i}`);
+}
+    const n = prompt("Digite seu nome: ");
+    const i = prompt("Digite sua idade: ");
+    
+nomeIdade(n, i);
