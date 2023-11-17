cef95fb40618811990aa3a35b218103c56b7b98f
lucaasfacanha <lucaspereirafacanha@gmail.com>
2023-11-17 10:58:56 -0300

    alteracao

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 import './style.css'
 
-function nomeIdade(nome, idade){
-    alert(`Nome: ${n}, Idade: ${i}`);
-}
-    const n = prompt("Digite seu nome: ");
-    const i = prompt("Digite sua idade: ");
-    
-nomeIdade(n, i);
