0c7fbdd83d9cf4b3fe2a2e4a73daa715fc023354
lucaasfacanha <lucaspereirafacanha@gmail.com>
2023-12-01 12:04:48 -0300

    a

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 import './style.css'
 
+const buttonBurguer = document.querySelector('.buttonBurguer');
+const categoriaMusica = document.querySelector('.CategoriaMusica');
+
+buttonBurguer.addEventListener('click', () => {
+    if (categoriaMusica.classList.contains('hidden')){
+        categoriaMusica.classList.remove('hidden')
+    } else {
+        categoriaMusica.classList.add('hidden')
+    }
+
+})
